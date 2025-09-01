796a36db9bdaccc7655e8afcc1e1eec7d835bdc9
Vitor Treglia <tregliavitor@gmail.com>
2025-09-01 20:30:26 -0300

    Initial commit

---
+++ Pessoa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vitor Aguiar Treglia on 31/08/25.
+//
+
+#include "Pessoa.h"
---
+++ Pessoa.h
@@ -0,0 +1,16 @@
+//
+// Created by Vitor Aguiar Treglia on 31/08/25.
+//
+
+#ifndef PESSOA_H
+#define PESSOA_H
+
+
+
+class Pessoa {
+
+};
+
+
+
+#endif //PESSOA_H
